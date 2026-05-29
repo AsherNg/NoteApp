eb1db56dd08a7cc7a75eec497fd930384a5ec41b
Asher Ng <asherng.sg@gmail.com>
2026-05-29 17:41:27 +0800

    Updated login and register page styling

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
-function InputField({ id, label, type, value, setter, error, onFocus, onBlur, isFocused }) {
-    const base = "text-black text-lg p-4 w-md text-clip rounded-lg border-2 box-border outline-none";
+function InputField({ id, label, type, value, setter, error, onFocus, onBlur, isFocused, example }) {
+    const base = "text-black text-base p-2 w-md text-clip rounded-lg border-2 box-border outline-none";
     const state = isFocused
         ? "border-blue-500 bg-gray-300"
         : error
@@ -10,8 +10,8 @@ function InputField({ id, label, type, value, setter, error, onFocus, onBlur, is
 
     return (
         <div className="flex flex-col items-start justify-center">
-            <div className="flex flex-row justify-start items-center gap-2">
-                <label htmlFor={id} className="text-gray-300 text-base">{ label }</label>
+            <div className="flex flex-row justify-start items-center">
+                <label htmlFor={id} className="my-2 text-base font-bold">{ label }</label>
                 {error && <span className="text-red-500 text-sm">{error}</span>}
             </div>
             <input
@@ -22,19 +22,20 @@ function InputField({ id, label, type, value, setter, error, onFocus, onBlur, is
                 onFocus={onFocus}
                 onBlur={onBlur}
                 className={`${base} ${state}`}
+                placeholder={example}
             />
         </div>
     );
 }
 
-function Button({ text, onClick, enabled = true, type = "button" }) {
+function Button({ text, onClick, enabled = true, type = "button", className }) {
     const base = "px-4 py-2 text-lg font-bold flex justify-center items-center rounded-lg box-border";
-    const enabledStyle = "text-white bg-green-500 hover:ring-4 hover:ring-green-700 active:bg-green-700 cursor-pointer";
+    const enabledStyle = "text-(--color-tertiary) bg-(--color-accent) hover:ring-4 hover:ring-green-700 active:bg-green-700 cursor-pointer";
     const disabledStyle = "text-white bg-gray-400 cursor-not-allowed opacity-60";
 
     return (
         <button
-            className={`${base} ${enabled ? enabledStyle : disabledStyle}`}
+            className={`${base} ${enabled ? enabledStyle : disabledStyle} ${className}`}
             onClick={onClick}
             disabled={!enabled}
             type={type}
@@ -44,9 +45,9 @@ function Button({ text, onClick, enabled = true, type = "button" }) {
     );
 }
 
-function Linker({ to, text }) {
+function Linker({ to, text, className }) {
     return (
-        <Link to={to} className="text-gray-400 text-xs hover:underline cursor-pointer">{text}</Link>
+        <Link to={to} className={`text-gray-400 text-xs hover:underline cursor-pointer ${className}`}>{text}</Link>
     )
 }
 
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import supabase from "./../../supabaseClient.jsx";
-import aginoteLogo from "./../../assets/aginoteLogo.png";
+import aginoteLogoOnly from "./../../assets/aginoteLogoOnly.png";
 import { InputField, Button, Linker } from "../../components/form.jsx";
+import { FcGoogle } from "react-icons/fc";
+import { ImGithub } from "react-icons/im";
 
 function Login() {
     const [email, setEmail] = useState('');
@@ -65,37 +67,55 @@ function Login() {
     const isFormClean = () => Object.keys(errors).length === 0;
 
     return (
-        <div className="flex flex-col justify-center items-center gap-2 w-full h-screen overflow-hidden bg-gray-900">
-            <img src={aginoteLogo} className="rounded-lg aspect-square w-xs" alt="AgiNote Logo" />
-            <form noValidate onSubmit={validateCredentials} className="flex flex-wrap justify-center items-start mx-4 border-box self-stretch border-b-2">
-                <div className="flex flex-wrap justify-around content-around gap-2 w-full">
-                    {[
-                        { id: "email", label: "E-mail", type: "email", value: email, setter: setEmail },
-                        { id: "password", label: "Password", type: "password", value: password, setter: setPassword }
-                    ].map(({ id, label, type, value, setter }) => (
-                        <InputField
-                            id={id}
-                            label={label}
-                            type={type}
-                            value={value}
-                            setter={setter}
-                            error={false}
-                            isFocused={focusedField === id}
-                            onFocus={() => handleFocus(id)}
-                            onBlur={handleBlur}
-                        />
-                    ))}
-                </div>
-                <div className="flex flex-col items-start justify-center gap-1 w-full mt-4 ml-4 pb-1">
-                    <Button text="Login!" type="submit" enabled={isFormClean()} />
-                    <Linker to="/register" text="Don't have an account?" />
-                    <Linker to="/recovery" text="Forget password?" />
-                </div>
-            </form>
-            <div className="flex flex-row justify-center items-start gap-2">
-                <Button text="Sign In With Google" onClick={handleGoogleSignIn} enabled={true}/>
-                <Button text="Sign In With Github" onClick={handleGitHubSignIn} enabled={true}/>
+        <div className="w-screen h-screen flex flex-col text-(--color-hover) bg-(--color-bg) justify-center">
+            <div className="flex flex-col items-center mb-10">
+                <img src={aginoteLogoOnly} alt="AgiNote Logo" className="w-32 h-32 my-2"/>
+                <span className="text-4xl font-bold">AgiNote</span>
             </div>
+            
+            <div className="flex flex-col items-center">
+                <span className="text-2xl font-semibold my-4">Login</span>
+
+                <form noValidate onSubmit={validateCredentials} className="flex flex-wrap justify-center items-start mx-4">
+                    <div className="flex flex-col items-center w-full">
+                        {[
+                            { id: "email", label: "Email", type: "email", value: email, setter: setEmail, example: "email@domain.com" },
+                            { id: "password", label: "Password", type: "password", value: password, setter: setPassword, example: "Enter your password" }
+                        ].map(({ id, label, type, value, setter, example }) => (
+                            <InputField
+                                id={id}
+                                label={label}
+                                type={type}
+                                value={value}
+                                setter={setter}
+                                error={false}
+                                isFocused={focusedField === id}
+                                onFocus={() => handleFocus(id)}
+                                onBlur={handleBlur}
+                                example={example}
+                            />
+                        ))}
+
+                        <Button className="my-4 w-md" text="Login" type="submit" enabled={isFormClean()} />
+                    </div>
+                    <div className="flex flex-row w-md justify-between px-4">
+                        <Linker to="/register" text="Don't have an account?" />
+                        <Linker to="/recovery" text="Forget password?" />
+                    </div>
+                </form>                
+            </div>
+
+            <div className="flex flex-col items-center justify-center">
+                <div className="flex flex-row items-center justify-center w-md my-2">
+                    <hr className="w-2/7"/>
+                    <span className="mx-4 text-base">or continue with</span>
+                    <hr className="w-2/7"/>
+                </div>
+                <div className="flex flex-row w-md items-center justify-center">
+                    <Button className="mx-4 w-20" text={<FcGoogle /> } onClick={handleGoogleSignIn} enabled={true}/>
+                    <Button className="mx-4 w-20" text={<ImGithub />} onClick={handleGitHubSignIn} enabled={true}/>
+                </div>
+            </div>  
         </div>
     )
 }
